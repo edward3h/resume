f5d0f81064145916dcdb48c6e5ad38a03bd88fcf
Edward Harman <jaq+github@ethelred.org>
2026-01-02 14:22:53 -0500

    Use pandoc to generate docx from html

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,12 +24,23 @@ var datestamp = tasks.register("datestamp") {
     }
 }
 
-var concatFilesTask = tasks.register<ConcatFilesTask>("concat") {
+var concatFiles = tasks.register<ConcatFilesTask>("concat") {
     dependsOn(datestamp)
     target = layout.buildDirectory.file("pages/index.html")
+    outputs.file(target)
     sources.from("src/html/header.html",
         markdown.get().into.file("resume.html"),
         if (project.hasProperty("includeContact")) markdown.get().into.file("contact.html") else null,
         datestamp.get().outputs.files.singleFile,
         "src/html/footer.html")
+}
+
+var docx = tasks.register<Exec>("docx") {
+    inputs.file(concatFiles.get().target)
+    outputs.file(layout.buildDirectory.file("docs/Edward_Harman_Resume.docx"))
+    commandLine = listOf("docker", "run", "--rm",
+        "--volume", layout.projectDirectory.asFile.absolutePath + ":/data", "pandoc/core:latest",
+        "--shift-heading-level-by=-1",
+        "-o", outputs.files.singleFile.relativeTo(layout.projectDirectory.asFile).toString(),
+        inputs.files.singleFile.relativeTo(layout.projectDirectory.asFile).toString())
 }
